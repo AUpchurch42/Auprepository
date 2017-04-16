fa67c2a01512f87ad0a4d1a19f339aefe73ea17d
AUpchurch42 <alexander.upchurch@gmail.com>
2017-04-15 20:19:20 -0400

    Add files via upload

---
+++ typings/index.js
@@ -0,0 +1,21 @@
+
+let Cat = function(name, color){
+  this.name = name;
+  this.color = color;
+}
+
+export default {
+  stringObj: '',
+  myString: '',
+  myNum: 1,
+  myDecimal: 0.02,
+  myFloat: 0.1,
+  myFloat2: 0.2,
+  myArrowFn: =>,
+  myObj: {},
+  Cat: Cat,
+  Fluffy: Cat,
+};
+
+
+
